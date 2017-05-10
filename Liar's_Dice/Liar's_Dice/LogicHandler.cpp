7b1b4bd7d58eb8b3f5d333f2ce6476b055bd1a6c
Ncpbeen <ncpbeena@avansnl>
2017-05-10 11:27:21 +0200

    Added fucntion vecContentEqual(vec,vec); and vecContains(vec,int);

--- Liar's_Dice/Liar's_Dice/LogicHandler.cpp
+++ Liar's_Dice/Liar's_Dice/LogicHandler.cpp
@@ -27,3 +27,26 @@ void spotOn(std::vector<int> bit, std::vector<int> current)
 {
 
 }
+
+
+
+// Check if elements of vec2 are in vec1
+// vec2 must be a equal or smaller size then vec1; vec2.size >= vec1.size
+bool LogicHandler::vecContentEqual(std::vector<int> vec1, std::vector<int> vec2) {
+
+	for (int i = 0; i < vec1.size(); i++) {
+		auto result = std::find(vec2.begin(), vec2.end(), vec1[i]);
+		if (result != vec2.end()) {
+			vec2.erase(result);
+		}
+		else
+			return false;
+
+		if (vec2.size() == 0)
+			return true;
+	}
+}
+
+bool LogicHandler::vecContains(std::vector<int>* vec, int toFind) {
+	return (std::find(vec->begin(), vec->end(), toFind) != vec->end());
+}
--- Liar's_Dice/Liar's_Dice/LogicHandler.h
+++ Liar's_Dice/Liar's_Dice/LogicHandler.h
@@ -4,5 +4,8 @@ class LogicHandler
 public:
 	LogicHandler();
 	~LogicHandler();
+
+	bool vecContentEqual(std::vector<int>, std::vector<int>);
+	bool vecContains(std::vector<int>*, int);
 };
 
