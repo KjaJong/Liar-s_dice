be9b22dd247831ac826edfc08f749be5cecb2e85
Ncpbeen <ncpbeena@avansnl>
2017-05-11 15:16:26 +0200

    LogiHandler callBluf(), soptOn, raise() and needed functions

--- Liar's_Dice/Liar's_Dice/LogicHandler.cpp
+++ Liar's_Dice/Liar's_Dice/LogicHandler.cpp
@@ -2,54 +2,111 @@
 #include <iostream>
 #include <vector>
 
-LogicHandler::LogicHandler()
-{
+using namespace std;
 
+
+// Checks if the new bid has a higher value then the last bid
+// Returns true if the new bid is a higher value
+bool raise(const vector<int>* lastBid, const vector<int>* newBid)
+{
+	//check if new bid is not empty
+	if (newBid->size() == 0)
+		return false;
+
+	//check if newBid containts valid numbers
+	if (!validDice(newBid))
+		return false;
+
+	//check if newBid contains out of the same values
+	if (!sameValues(newBid))
+		return false;
+
+	//check if the new value of eyes is higher
+	if (lastBid[0] < newBid[0]) {
+		
+			return true;
+	}
+	//check if the eyes value 
+	else if (lastBid[0] == newBid[0]) {
+		if (lastBid->size() < newBid->size())
+			return true;
+	}
+
+	return false;
 }
 
-
-LogicHandler::~LogicHandler()
+// Checks if the bid is incorrect. 
+// Returns true if bid is incorrrect.
+bool callBluff(vector<int>* actual, vector<int>* bid)
 {
-
+	return !containsElements(*actual, *bid);
 }
 
-void raise(std::vector<int> bit, std::vector<int> current)
+// Checks if the bid is correct. 
+// Returns true if bid is correct.
+bool spotOn(vector<int>* actual, vector<int>* bid)
 {
-
-}
-
-void callBluff(std::vector<int> bit, std::vector<int> current)
-{
-
-}
-
-void spotOn(std::vector<int> bit, std::vector<int> current)
-{
-
+	return containsElements(*actual, *bid);
 }
 
 
 
 // Check if the content of vector 2 are in the content of vector 1
 // Returns true if all elements of vector 2 are in vector 1. Therefor if the size of vector 2 is bigger than vector 1 return false
-// ex: vec1[5, 2, 5]; vec2[5,5]; Checks if the two fives from vec2 are in vec1; vec1 contains the elements of vec2 so return true
-bool LogicHandler::vecContainsElements(std::vector<int> vec1, std::vector<int> vec2) {
-	if (vec2.size() > vec1.size())
+// ex: base[5, 2, 5]; comparison[5,5]; Checks if the two fives from comparison are in base; base contains the elements of comparison so return true
+bool containsElements( vector<int> base, vector<int> comparison) {
+	if (comparison.size() == 0)
+		cout << "Error: Vector comparison has no elements \n\t@LogicHandler.cpp \n\t@vecContainsElements(vector<int>, vector<int>)" << endl;
+
+	if (comparison.size() > base.size())
 		return false;
 
-	for (int i = 0; i < vec1.size(); i++) {
-		auto result = std::find(vec2.begin(), vec2.end(), vec1[i]);
-		if (result != vec2.end()) {
-			vec2.erase(result);
+	for (int i = comparison.size() -1; i >= 0 ; i--) {
+	//for (int i =  0; i < comparison.size(); i++) {
+		auto result = find(base.begin(), base.end(), comparison[i]);
+		
+		if (result != base.end()) {
+			base.erase(result);
+			comparison.erase(comparison.begin() + i);
 		}
 		else
 			return false;
 
-		if (vec2.size() == 0)
+		if (comparison.size() == 0)
 			return true;
 	}
 }
 
-bool LogicHandler::vecContains(std::vector<int>* vec, int toFind) {
-	return (std::find(vec->begin(), vec->end(), toFind) != vec->end());
+bool vecContains(vector<int>* vec, int toFind) {
+	return (find(vec->begin(), vec->end(), toFind) != vec->end());
+}
+
+
+// Checks if all the values in the given vector are the same
+// Return true when all values are the same
+// ex:	vector{ 5, 5, 5} returns true
+//		vector{ 5, 5, 2} returns false
+bool sameValues(const vector<int>* vec) {
+	int number = vec->at(0);
+	for (int i = 0; i < vec->size(); i++) {
+		if (vec->at(i) != number)
+			return false;
+	}
+	return true;
+}
+
+// Check if the vector array filled with numbers are valid 6 sided die numbers
+bool validDice(const vector<int>* dice) {
+	for (int i = 0; i < dice->size(); i++) {
+		if (!validDie(dice->at(i)))
+			return false;
+	}
+	return true;
+}
+
+// Check if the number is a valid 6 sided die number
+bool validDie(int die) {
+	if (die < 1 || die > 6)
+		return false;
+	return true;
 }
--- Liar's_Dice/Liar's_Dice/LogicHandler.h
+++ Liar's_Dice/Liar's_Dice/LogicHandler.h
@@ -1,11 +1,14 @@
 #pragma once
-class LogicHandler
-{
-public:
-	LogicHandler();
-	~LogicHandler();
+#include <vector>
 
-	bool vecContainsElements(std::vector<int>, std::vector<int>);
-	bool vecContains(std::vector<int>*, int);
-};
 
+bool raise(const std::vector<int>*, const std::vector<int>*);
+bool callBluff(std::vector<int>*, std::vector<int>*);
+bool spotOn(std::vector<int>*, std::vector<int>*);
+
+
+bool containsElements(std::vector<int>, std::vector<int>);
+bool vecContains(std::vector<int>*, int);
+bool sameValues(const std::vector<int>*);
+bool validDice(const std::vector<int>*);
+bool validDie(int);
