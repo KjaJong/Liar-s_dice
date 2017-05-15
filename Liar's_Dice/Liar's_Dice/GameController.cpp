5ab1c6521d04cda4d5eca8f684526d98f0b7de00
Tom Remeeus <info@computerinfor.nl>
2017-05-15 14:01:42 +0200

    Init

--- Liar's_Dice/Liar's_Dice/GameController.cpp
+++ Liar's_Dice/Liar's_Dice/GameController.cpp
@@ -9,9 +9,14 @@
 using std::vector;
 using std::string;
 
+LogicHandler LH;
+vector<int> curBid;
+vector<int> newBid;
+
 GameController::GameController(vector<Player> list)
 {
 	players = list;
+	LH = LogicHandler();
 }
 
 GameController::~GameController()
@@ -25,20 +30,8 @@ void GameController::pickFirstPlayer()
 	int pick = rand() % players.size() + 1;
 	curPlayer = pick - 1;
 	
-	//set bet
-	string bet;
 	std::cout << "Player " << players[curPlayer].getName() << " has to begin the game." << std::endl;
-	std::cout << "Please place your bet. Seperate your numbers with a comma (1,2,3,4): ";
-	std::cin >> bet;
-
-	//convert input to a vector of strings
-	vector<string> numbers;
-	std::istringstream split(bet);
-	string number;
-	while (getline(split, number, ','))
-	{
-		numbers.push_back(number);
-	}
+	curBid = setBet();
 }
 
 void GameController::pickNextPlayer()
@@ -76,7 +69,25 @@ void GameController::deletePlayer(int index)
 
 void GameController::turn()
 {
-	
+	int choice;
+
+	std::cout << "Choose an action:" << std::endl;
+	std::cout << "*Press 1 to raise current bid" << std::endl;
+	std::cout << "*Press 2 to call spot on" << std::endl;
+	std::cout << "*Press 3 to call bluff" << std::endl;
+
+	std::cin >> choice;
+
+	switch (choice)
+	{
+		case 1:
+			raise();
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+	}
 }
 
 void GameController::rollDice()
@@ -96,4 +107,47 @@ void GameController::rollDice()
 
 		std::cout << std::endl;
 	}
+}
+
+void GameController::raise()
+{
+	bool check;
+
+	newBid = setBet();
+	check = LH.raise(&curBid, &newBid);
+
+	if (check)
+	{
+		std::cout << "yes me friend" << std::endl;
+	}
+	else
+	{
+		std::cout << "no no no sir" << std::endl;
+	}
+}
+
+void GameController::callBluff()
+{
+
+}
+
+vector<int> GameController::setBet()
+{
+	//set bet
+	string bet;
+	
+	std::cout << "Please place your bet." << std::endl;
+	std::cin >> bet;
+
+	//convert input to a vector of ints
+	vector<int> numbers;
+	std::istringstream split(bet);
+	string number;
+	while (getline(split, number, ','))
+	{
+		int value = std::atoi(number.c_str());
+		numbers.push_back(value);
+	}
+
+	return numbers;
 }
--- Liar's_Dice/Liar's_Dice/GameController.h
+++ Liar's_Dice/Liar's_Dice/GameController.h
@@ -13,6 +13,11 @@ private:
 	int curPlayer;
 	vector<int> curBid;
 	vector<Player> players;
+
+	vector<int> setBet();
+	void raise();
+	void callBluff();
+	void spotOn();
 	
 public:
 	GameController(vector<Player> players);
