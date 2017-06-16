47690a41e9a16a3e30b89540b95a4a45040a101b
Tom Remeeus <info@computerinfor.nl>
2017-06-16 16:57:04 +0200

    SFX update 2

--- Liar's_Dice/Liar's_Dice/SFX.cpp
+++ Liar's_Dice/Liar's_Dice/SFX.cpp
@@ -27,8 +27,9 @@ SFX::~SFX()
 //correct guess sound effects
 void SFX::setWin()
 {
-	win.push_back("media/sfx/correct/Deez nuts.mp3");
+	win.push_back("media/sfx/correct/Deez Nuts.mp3");
 	win.push_back("media/sfx/correct/Gotcha.mp3");
+	win.push_back("media/sfx/correct/Noice.mp3");
 	win.push_back("media/sfx/correct/Right ding.mp3");
 	win.push_back("media/sfx/correct/Victory.mp3");
 }
@@ -36,8 +37,9 @@ void SFX::setWin()
 //wrong guess sound effects
 void SFX::setLose()
 {
-	lose.push_back("media/sfx/wrong/Losing horn.mp3");
+	lose.push_back("media/sfx/wrong/Cry.mp3");
 	lose.push_back("media/sfx/wrong/Fart.mp3");
+	lose.push_back("media/sfx/wrong/Losing horn.mp3");
 	lose.push_back("media/sfx/wrong/Nope.mp3");
 	lose.push_back("media/sfx/wrong/Punch.mp3");
 	lose.push_back("media/sfx/wrong/Wilhelm scream.mp3");
@@ -48,6 +50,7 @@ void SFX::setGameOver()
 {
 	gameOver.push_back("media/sfx/game over/Beep.mp3");
 	gameOver.push_back("media/sfx/game over/Mario.mp3");
+	gameOver.push_back("media/sfx/game over/Not pass.mp3");
 	gameOver.push_back("media/sfx/game over/PacMan.mp3");
 	gameOver.push_back("media/sfx/game over/Shutdown.mp3");
 }
